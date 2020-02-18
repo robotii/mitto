89134374672e80bb2eae669382fd73e2a4f01c53
Peter Arthur <pete.arthur@gmail.com>
2020-02-18 21:58:41 +0000

    Lexer interface and first tokens added

--- compiler/lexer/lexer.go
+++ compiler/lexer/lexer.go
@@ -1,13 +1,21 @@
 package lexer
 
-import "mitto/fsm"
+import (
+	"mitto/compiler/token"
+	"mitto/fsm"
+)
 
-// Lexer structure used to convert programs into a stream of tokens.
+// Lexer the interface between the parser and the lexer
+type Lexer interface {
+	NextToken() token.Token
+}
+
+// mLexer structure used to convert programs into a stream of token.
 //
 // Input is an array of runes, and processing is done assuming the
 // text is encoded in UTF8. There is a FSM, which is used to do some
 // context-sensitive lexing.
-type Lexer struct {
+type mLexer struct {
 	input        []rune   // the current input buffer being lexed
 	position     int      // the index of the character we are currently lexing
 	readPosition int      // the index of the next character to be read
@@ -21,8 +29,8 @@ const (
 )
 
 // New initializes a new lexer with input string
-func New(input string) *Lexer {
-	l := &Lexer{input: []rune(input)}
+func New(input string) *mLexer {
+	l := &mLexer{input: []rune(input)}
 	l.advance()
 
 	l.fsm = fsm.New(
@@ -32,10 +40,24 @@ func New(input string) *Lexer {
 	return l
 }
 
+// NextToken makes lexer tokenize next character(s)
+func (l *mLexer) NextToken() token.Token {
+
+	var tok token.Token
+
+	switch l.ch {
+	default:
+		tok = token.Create(token.Illegal, string(l.ch), l.line)
+	}
+
+	l.advance()
+	return tok
+}
+
 // advance sets the current character and advances the read position.
 // If the position exceeds the bounds of the input, the current
 // character is set to null.
-func (l *Lexer) advance() {
+func (l *mLexer) advance() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
--- compiler/token/tokens.go
+++ compiler/token/tokens.go
@@ -1,4 +1,4 @@
-package tokens
+package token
 
 // Type is used to determine what type of token we have.
 // It is aliased here for convenience.
@@ -10,4 +10,15 @@ type Token struct {
 	Type    Type   // the type of the token
 	Literal string // the literal string value of the token as lexed
 	Line    int    // the line number the token appeared on
+}
+
+// Literals
+const (
+	Illegal = "ILLEGAL"
+	EOF     = "EOF"
+)
+
+// Create will create a token
+func Create(t Type, literal string, line int) Token {
+	return Token{Type: t, Literal: literal, Line: line}
 }
