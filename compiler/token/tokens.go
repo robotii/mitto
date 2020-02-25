f2fb8286d99e774c366232e1e1e08465f2985e41
Peter Arthur <pete.arthur@gmail.com>
2020-02-25 22:04:26 +0000

    List of tokens that we expect to lex

--- compiler/token/tokens.go
+++ compiler/token/tokens.go
@@ -14,9 +14,163 @@ type Token struct {
 
 // Literals
 const (
-	Illegal = "ILLEGAL"
-	EOF     = "EOF"
+	Illegal          = "ILLEGAL"
+	EOF              = "EOF"
+	Constant         = "CONSTANT"
+	Ident            = "IDENT"
+	InstanceVariable = "INSTANCE_VAR"
+	Int              = "INT"
+	Float            = "FLOAT"
+	String           = "STRING"
+	Comment          = "COMMENT"
+
+	Assign   = "="
+	Plus     = "+"
+	PlusEq   = "+="
+	Minus    = "-"
+	MinusEq  = "-="
+	Bang     = "!"
+	Asterisk = "*"
+	Pow      = "**"
+	Slash    = "/"
+	Dot      = "."
+	And      = "&&"
+	Or       = "||"
+	OrEq     = "||="
+	Modulo   = "%"
+
+	Match = "=~"
+	LT    = "<"
+	LTE   = "<="
+	GT    = ">"
+	GTE   = ">="
+	COMP  = "<=>"
+
+	Comma     = ","
+	Semicolon = ";"
+	Colon     = ":"
+	Bar       = "|"
+	Amp       = "&"
+
+	LParen   = "("
+	RParen   = ")"
+	LBrace   = "{"
+	RBrace   = "}"
+	LBracket = "["
+	RBracket = "]"
+
+	Eq        = "=="
+	NotEq     = "!="
+	IsSame    = "==="
+	IsNotSame = "!=="
+	Range     = ".."
+	RangeExcl = "..."
+
+	True     = "TRUE"
+	False    = "FALSE"
+	Null     = "Null"
+	If       = "IF"
+	ElsIf    = "ELSIF"
+	Else     = "ELSE"
+	Default  = "DEFAULT"
+	Switch   = "SWITCH"
+	Case     = "CASE"
+	Return   = "RETURN"
+	Next     = "NEXT"
+	Break    = "BREAK"
+	Def      = "DEF"
+	Self     = "SELF"
+	Super    = "SUPER"
+	While    = "WHILE"
+	Yield    = "YIELD"
+	GetBlock = "BLOCK"
+	HasBlock = "HASBLOCK"
+	Class    = "CLASS"
+	Module   = "MODULE"
+	Catch    = "CATCH"
+	Finally  = "FINALLY"
+
+	ResolutionOperator = "::"
+
+	RightArrow = "->"
+	LeftArrow  = "<-"
 )
+
+var keywords = map[string]Type{
+	"def":     Def,
+	"true":    True,
+	"false":   False,
+	"nil":     Null,
+	"if":      If,
+	"elsif":   ElsIf,
+	"else":    Else,
+	"switch":  Switch,
+	"case":    Case,
+	"default": Default,
+	"return":  Return,
+	"self":    Self,
+	"super":   Super,
+	"while":   While,
+	"yield":   Yield,
+	"next":    Next,
+	"class":   Class,
+	"module":  Module,
+	"break":   Break,
+	"block!":  GetBlock,
+	"block?":  HasBlock,
+	"catch":   Catch,
+	"finally": Finally,
+}
+
+var operators = map[string]Type{
+	"=":   Assign,
+	"+":   Plus,
+	"+=":  PlusEq,
+	"-":   Minus,
+	"-=":  MinusEq,
+	"!":   Bang,
+	"*":   Asterisk,
+	"**":  Pow,
+	"/":   Slash,
+	".":   Dot,
+	"&&":  And,
+	"||":  Or,
+	"||=": OrEq,
+	"%":   Modulo,
+	"&":   Amp,
+
+	"=~":  Match,
+	"<":   LT,
+	"<=":  LTE,
+	">":   GT,
+	">=":  GTE,
+	"<=>": COMP,
+
+	"==":  Eq,
+	"!=":  NotEq,
+	"===": IsSame,
+	"!==": IsNotSame,
+	"..":  Range,
+	"...": RangeExcl,
+
+	"::": ResolutionOperator,
+	"->": RightArrow,
+	"<-": LeftArrow,
+}
+
+var separators = map[string]Type{
+	",": Comma,
+	";": Semicolon,
+	":": Colon,
+	"|": Bar,
+
+	"(": LParen,
+	")": RParen,
+	"{": LBrace,
+	"}": RBrace,
+	"[": LBracket,
+	"]": RBracket,
+}
 
 // Create will create a token
 func Create(t Type, literal string, line int) Token {
